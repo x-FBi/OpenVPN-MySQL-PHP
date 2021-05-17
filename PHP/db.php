af1706db736e345997d15cd3b31246abc4adcff6
FBi <36416072+x-FBi@users.noreply.github.com>
2021-05-17 01:46:25 -0400

    Update db.php

--- PHP/db.php
+++ PHP/db.php
@@ -1,6 +1,12 @@
 <?php
+#### All These Setting are to be copied from the Python Config.py ####
+$hostname='127.0.0.1'; #IP or host of main server
+$databasename='users'; #Database Name
+$username='admin'; # User you created in MySQL
+$password='password1'; # Password for MySQL user
+####
 try {
-        $db = new PDO('sqlite:database.name');
+        $db = new PDO('mysql:host=$hostname;dbname=$databasename', $username, $password);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 } catch (PDOException $e) {
